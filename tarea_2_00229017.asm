07b9cb57c69b70ad2ef8e7018d7f7339fc57abe9
kaztro <00229017@uca.edu.sv>
2020-04-25 23:30:41 -0600

    Se arreglo problema con el primero y segundo ejercicio...

--- tarea_2_00229017.asm
+++ tarea_2_00229017.asm
@@ -3,36 +3,38 @@ org 100h
 ;primceller ejercicio: 00229017-> (2+2+9+0+1+7)/6 -> 21/6 = 3.5
 
 
-mov ax, 0000h
+;mov ax, 0000h
 
-add ax, 2d
-add ax, 2d
-add ax, 9d
-add ax, 1d
-add ax, 7d
+;add ax, 2d
+;add ax, 2d
+;add ax, 9d
+;add ax, 1d
+;add ax, 7d
 
-mov cl, 6d
+;mov cl, 6d
 
-div cl ;Rrespuesta=3 -> Hay salud
+;div cl ;Rrespuesta=3 -> Hay salud
 
-mov cl, "H"
-mov [200h], cl
-mov cl, "a"
-mov [201h], cl
-mov cl, "y"
-mov [203h], cl
-mov cl, "s"
-mov[204h], cl
-mov cl, "a"
-mov [205h], cl
-mov cl, "l"
-mov [206h], cl
-mov cl, "u"
-mov [207h], cl
-mov cl, "d"
-mov [208h], cl
+;mov cl, "H"
+;mov [200h], cl
+;mov cl, "a"
+;mov [201h], cl
+;mov cl, "y"
+;mov [202h], cl
+;mov cl, ""
+;mov [203h], cl
+;mov cl, "s"
+;mov[204h], cl
+;mov cl, "a"
+;mov [205h], cl
+;mov cl, "l"
+;mov [206h], cl
+;mov cl, "u"
+;mov [207h], cl
+;mov cl, "d"
+;mov [208h], cl
 
-;Segcellundo ejercicio: 
+;Segundo ejercicio: 
 
 mov     ax, 0000h
 mov     al, 2d
@@ -52,5 +54,5 @@ segcell: add bx, 2h
 primcell: add bx, 1h
 
 n: cmp bx, 21Fh
-   jb estTot
-   int 10h
+    jb estTot
+    int 10h
