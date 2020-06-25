c5f51c9f7d8ef55909d49432ab4eb0821c5fec81
kaztro <00229017@uca.edu.sv>
2020-06-25 16:16:53 -0600

    Casi casi

--- tarea_5_00229017/tarea_5_00229017.asm
+++ tarea_5_00229017/tarea_5_00229017.asm
@@ -7,31 +7,53 @@ section .text
 	xor 	si, si
 	xor 	di, di
 
-	mov 	bx,100d	; Lleva el control de cuando parar
-
-	mov 	si, 25d ; X -> Columna
+	mov 	si, 115d ; X -> Columna
 	mov 	di, 25d ; Y -> Fila
 	call 	linea_h
 
-	mov 	si, 25d  ; X -> Columna
-	mov 	di, 125d ; Y -> Fila
+	mov 	si, 145d ; X -> Columna
+	mov 	di, 55d ; Y -> Fila
 	call 	linea_h
 
-	mov 	bx,125d
-	mov	    si, 25d ; X -> Columna
+	mov 	si, 155d ; X -> Columna
+	mov 	di, 75d ; Y -> Fila
+	call 	linea_h
+    
+	mov 	si, 115d ; X -> Columna
+	mov 	di, 175d ; Y -> Fila
+	call 	linea_h
+
+    mov 	si, 155d ; X -> Columna
+	mov 	di, 95d ; Y -> Fila
+	call 	linea_h2
+    
+    mov 	si, 145d ; X -> Columna
+	mov 	di, 145d ; Y -> Fila
+	call 	linea_h2
+
+	mov	    si, 115d ; X -> Columna
 	mov 	di, 25d ; Y -> Fila
 	call 	linea_v
 
-	mov 	bx, 45d 
-	mov	    si, 100d ; X -> Columna
-	mov 	di, 25d  ; Y -> Fila
+	mov	    si, 205d ; X -> Columna
+	mov 	di, 75d ; Y -> Fila
 	call 	linea_v
 
-	mov 	bx, 126d 
-	mov	    si, 100d ; X -> Columna
-	mov 	di, 105d ; Y -> Fila
-	call 	linea_v
+	mov	    si, 205d ; X -> Columna
+	mov 	di, 25d ; Y -> Fila
+	call 	linea_v2
 
+	mov	    si, 145d ; X -> Columna
+	mov 	di, 55d ; Y -> Fila
+	call 	linea_v3
+
+    mov	    si, 185d ; X -> Columna
+	mov 	di, 95d ; Y -> Fila
+	call 	linea_v3
+
+	mov	    si, 155d ; X -> Columna
+	mov 	di, 75d ; Y -> Fila
+	call 	linea_v4
 
 	call 	kb		; Utilizamos espera de alguna tecla
 
@@ -53,8 +75,18 @@ lupi_h:	mov 	cx, 0d ; Columna
 	mov	dx, di ; Fila
 	call 	pixel
 	inc 	si
-	cmp 	si, bx
+	cmp 	si, 205d
 	jne 	lupi_h
+	ret
+
+linea_h2: 
+lupi_h2:mov 	cx, 0d ; Columna 
+	add 	cx, si
+	mov	dx, di ; Fila
+	call 	pixel
+	inc 	si
+	cmp 	si, 185d
+	jne 	lupi_h2
 	ret
 
 linea_v:
@@ -63,11 +95,42 @@ lupi_v:	mov 	cx, si ; Columna
 	add 	dx, di
 	call 	pixel
 	inc 	di
-	cmp 	di, bx
+	cmp 	di, 175d
 	jne 	lupi_v
 	ret
 
+linea_v2:
+lupi_v2:mov 	cx, si ; Columna 
+	mov	dx, 0d ; Fila
+	add 	dx, di
+	call 	pixel
+	inc 	di
+	cmp 	di, 55d
+	jne 	lupi_v2
+	ret
+
+linea_v3:
+lupi_v3:mov 	cx, si ; Columna 
+	mov	dx, 0d ; Fila
+	add 	dx, di
+	call 	pixel
+	inc 	di
+	cmp 	di, 145d
+	jne 	lupi_v3
+	ret
+
+linea_v4:
+lupi_v4:mov 	cx, si ; Columna 
+	mov	dx, 0d ; Fila
+	add 	dx, di
+	call 	pixel
+	inc 	di
+	cmp 	di, 95d
+	jne 	lupi_v4
+	ret
 
 kb: 	mov	ah, 00h
 	int 	16h
 	ret
+
+section .data
